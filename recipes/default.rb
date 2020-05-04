6a4d1399778a284be9e9adadec86840a4f3f23d6
elaouane <hamzagamedev@hotmail.com>
2020-05-05 00:42:32 +0100

    enabled http

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,6 +19,10 @@ execute 'enable_module_system' do
   command 'sudo metricbeat modules enable system'
 end
 
+execute 'enable_module_system' do
+  command 'sudo metricbeat modules enable http'
+end
+
 
 execute 'download_filebeat' do
   command 'curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.6.1-amd64.deb'
