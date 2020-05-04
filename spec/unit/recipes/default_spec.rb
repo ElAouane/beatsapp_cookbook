a18735bc6072a6738c1f91470ae7e46c7fe0861a
elaouane <hamzagamedev@hotmail.com>
2020-05-04 21:44:20 +0100

    changed cookbook

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,7 +6,7 @@
 
 require 'spec_helper'
 
-describe 'beatsapp2_cookbook::default' do
+describe 'beatsapp::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
